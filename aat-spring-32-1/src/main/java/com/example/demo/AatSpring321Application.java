dd664afb999986719454fb00417c73c38834f303
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-11-03 16:32:39 +0100

    first commit

---
+++ aat-spring-32-1/src/main/java/com/example/demo/AatSpring321Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AatSpring321Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AatSpring321Application.class, args);
+	}
+
+}
